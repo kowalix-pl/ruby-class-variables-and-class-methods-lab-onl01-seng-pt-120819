4b192609bd57ed66c061b51a18a06dc4bd086729
kowalix007 <kowalix007@gmail.com>
2020-01-10 23:53:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song 
+@@count = 0
+@@artists = []
+@@genres = []
+@@genre_count = {}
+
+ attr_accessor :name, :artist, :genre
+def initialize (name,artist,genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+  @@genres << genre
+  @@artists << artist
+end 
+
+def self.count
+  @@count 
+end 
+def self.genres
+  @@genres.uniq!
+end
+
+def self.artists
+  @@artists.uniq!
+end
+
+
+def self.genre_count
+    
+    @@genres.each do |genre|
+   if @@genre_count[genre]
+        @@genre_count[genre] += 1
+        
+end
+end
+end 
+end 
+ 
+ 
+ 
